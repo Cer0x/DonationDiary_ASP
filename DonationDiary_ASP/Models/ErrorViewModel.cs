1a6ceb93a7963228e7cebbfd6bcbf905717f584a
Patryk Gnutek <patryk.gnutek@protonmail.com>
2023-10-13 20:42:31 +0200

    Dodaj pliki projektów.

---
+++ DonationDiary_ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DonationDiary_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
